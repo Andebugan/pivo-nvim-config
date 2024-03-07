1780e9bb65481fbf4bd2c36b96c146ccddc11ed4
andreibugaenko2012@mail.ru <andreibugaenko2012@mail.ru>
2024-03-07 14:15:23 +0300

    finished csharp config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,7 +13,9 @@ return {
         "vim",
         "vimdoc",
         "yaml",
-        "markdown"
+        "markdown",
+        -- csharp
+        "c_sharp"
       },
       highlight = { enable = true },
       indent = { enable = true }
