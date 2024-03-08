d8591dc49283cdd0e133f0bf951b68f87c44e9bb
andreibugaenko2012@mail.ru <andreibugaenko2012@mail.ru>
2024-03-08 10:27:32 +0300

    added session picker

---
+++ lua/plugins/session.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'rmagatti/auto-session',
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      }
+    end
+  },
+  {
+    'rmagatti/session-lens',
+    requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
+    config = function()
+      require('session-lens').setup({})
+
+      vim.keymap.set('n', '<leader>ss', ':SearchSession<CR>', {})
+    end
+  }
+}
