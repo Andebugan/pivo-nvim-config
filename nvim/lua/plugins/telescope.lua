e7ad100dd2ccff264b0cdd34f170d0688aecd617
andebugan <andreibugaenko2012@mail.ru>
2024-05-06 23:26:05 +0000

    added neo-tree

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,24 +1,37 @@
 return {
     {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    }
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    }
+                }
+            }
+
+            require("telescope").load_extension("ui-select")
+        end
+    },
+    {
+        'nvim-telescope/telescope-project.nvim',
+        config = function()
+            require("telescope").load_extension('project')
+        end
+    },
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.6', 
         dependencies = {
             'nvim-lua/plenary.nvim'
-            'nvim-telescope/telescope-fzf-native.nvim',
-            'nvim-telescope/telescope-ui-select.nvim'
         },
         config = function()
-            cal builtin = require("telescope.builtin")
+            local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
             vim.keymap.set('n', '<leader>fp', ':Telescope projects<CR>', {})
-            vim.keymap.set('n', '<leader>pr', ':ProjectRoot<CR>', {})
             require('telescope').setup {
                 file_ignore_patterns = {
                     "node_modules/.*",
@@ -26,22 +39,7 @@ return {
                     "tmp/.*",
                     "cahce/.*"
                 },
-                extensions = {
-                    fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                        case_mode = "smart_case",
-                    },
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {}
-                    }
-                }
             }
-
-            require('telescope').load_extension('fzf')
-            require('telescope').load_extension('ui-select')
-            require('telescope').load_extension('project')
         end
     },
 }
