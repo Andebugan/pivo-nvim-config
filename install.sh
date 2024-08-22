c4b95b78dd5321d4a703cb2acb57fba684239459
andebugan <andreibugaenko2012@mail.ru>
2024-08-22 19:34:27 +0300

    moved sudo alias into Dockerfile

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# sudo ignorer for executing bash commands
-echo 'if [ $(id -u) -eq 0]; then' >> ~/.bashrc\
-    && echo '   alias sudo=eval' >> ~/.bashrc\
-    && echo 'fi' >> ~./bashrc
-
 # temporary installer for pivodev
 echo "export PIVODIR=~/.pivodev" >> ~/.bashrc
 echo 'export PATH=$PATH:~/.pivodev/bin' >> ~/.bashrc
