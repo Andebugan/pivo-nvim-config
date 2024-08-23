9540af2f069aba8fa23b96cd496618e47627adf9
andebugan <andreibugaenko2012@mail.ru>
2024-08-23 18:27:44 +0000

    fixed csharp.sh

--- scripts/csharp.sh
+++ scripts/csharp.sh
@@ -3,7 +3,6 @@
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb\
     && sudo dpkg -i packages-microsoft-prod.deb\
     && sudo rm packages-microsoft-prod.deb\
-
     && sudo apt-get update\
     && sudo apt-get install dotnet-sdk-8.0 dotnet-runtime-8.0 aspnetcore-runtime-8.0 zip -y\
     && sudo apt autoremove -y
